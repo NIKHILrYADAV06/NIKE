12addeb70cebeb22fb8555be4c28d6323cb0e847
Manav Yadav <yadavmanav1304@gmail.com>
2026-01-24 13:56:12 +0530

    messages-screen

--- Mobile/app/(tabs)/index.tsx
+++ Mobile/app/(tabs)/index.tsx
@@ -4,9 +4,9 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import SignOutButton from '@/components/SignOutButton'
 const HomeScreen = () => {
     return (
-        <SafeAreaView className="flex-1 bg-white">
+        <SafeAreaView className="flex-1 bg-[#0F1A24]">
             <View className="flex-1 items-center justify-center">
-                <Text>HomeScreen</Text>
+                <Text className="text-[#1DA1F2]">HomeScreen</Text>
                 <SignOutButton />
             </View>
         </SafeAreaView>
